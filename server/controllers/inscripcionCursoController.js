c7074d90bc43b2f840eb1dfd933b5dbe1d7a6f5e
David Muñoz <davidmunozmoreno2@gmail.com>
2025-10-24 11:12:02 -0500

    feat: Ya se pueden inscribir empleados a un curso (probar en el postman)

--- server/controllers/inscripcionCursoController.js
+++ server/controllers/inscripcionCursoController.js
@@ -4,6 +4,7 @@ const InscripcionCurso = require("../models/InscripcionCurso");
 const Curso = require("../models/curso");
 const Usuario = require("../models/User");
 const { json } = require("sequelize");
+const e = require("express");
 
 const crearOActualizarInscripcion = async (req, res) => {
   const { curso_ID, aprendiz_ID, nuevoEstado } = req.body;
@@ -84,7 +85,7 @@ const crearOActualizarInscripcion = async (req, res) => {
 const inscripcionEmpleados = async (req, res ) => {
     try{
       const {empleados, curso_ID, gestor_ID} = req.body;
-      
+      let verificarCursos= {}
 
       if (Object.keys(empleados).length < 0) {
         return res.status(400).json({
@@ -136,28 +137,62 @@ const inscripcionEmpleados = async (req, res ) => {
         .flat()
         .filter(Boolean)
         .map(item => item.dataValues);
-
+        
        const horiarioCursos = await Promise.all(
           filtrar.map(async (f) =>{
             const consult = await Curso.findByPk(f.curso_ID,{
               attributes : ['slots_formacion']
           })
-            return consult.dataValues
+            return{
+              ID : f.aprendiz_ID,
+              horarios : consult.dataValues.slots_formacion
+            }
           })
        )
-       
-       const verificarCursos = await Promise.all(
+       const mensaje = "No se puede estar inscrito a un curso con los mismos horarios de formacion"
+        verificarCursos = await Promise.all(
         horiarioCursos.map(async (h) =>{
-          const horarios = JSON.parse(h.slots_formacion)
+          const horarios = JSON.parse(h.horarios)
 
           const verificar = horarios.some(h => arrayCurso.includes(h))
-          return verificar
+          
+          return {
+            ID : h.ID,
+            verificar,
+            mensaje
+          }
         })
        )
-       console.log(verificarCursos)
+       verificarCursos.map( (v) =>{
+        if (v.verificar) {
+          for(let i = 0; i < empleados.length; i++){
+            if (empleados[i].ID === v.ID) {
+              empleados.splice(i,1)
+              i--;
+            }
+          }
+        }
+       })
       }
+      if (empleados.length <= 0) {
+        return res.status(409).json({
+          message: "No se puede inscribir: los empleados ya están asignados a cursos con los mismos horarios de formación."
+        });
+      }
+      const result = await Promise.all(
+        empleados.map(async (e) =>{
+          const inscribir = await InscripcionCurso.create({
+            fecha_inscripcion : new Date(),
+            aprendiz_ID : e.ID,
+            curso_ID : curso_ID,
+            gestor_ID: gestor_ID
+          })
+          return inscribir
+        })
+      )
       res.status(200).json({
-        message : 'melo'
+        message : 'Se inscribieron los empleados al curso',
+        noInscritos: verificarCursos
       })
     } catch (error){
       console.error('Error al inscribir los empleados', error)
